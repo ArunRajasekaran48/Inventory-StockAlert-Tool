d43503984e326df8ccc4ddaf5ff743d9cf3fa120
ArunRajasekaran48 <arun.r2023cse@sece.ac.in>
2025-07-25 14:34:30 +0530

    Cors fix

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,15 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(),react()],
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-        secure: false,
-      }
-    }
-  }
+  plugins: [tailwindcss(),react()]
 })
